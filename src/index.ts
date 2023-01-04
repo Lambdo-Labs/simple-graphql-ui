9fca6416ee7f1dad286d6e548b2b0d66d7de43f9
Andre Carrera <andre@carrera.red>
2023-01-04 16:33:09 -0700

    fix: component registration

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,11 @@ import FieldSelectCheckboxes from "./components/tree/FieldSelectCheckboxes.vue";
 import FieldSelectOption from "./components/tree/FieldSelectOption.vue";
 import type { App } from "vue";
 
-const components = {
-  GraphqlSimpleExplorer,
-  FieldSelectCheckboxes,
-  FieldSelectOption,
-};
-
 const install = {
   install(Vue: App) {
-    for (const prop in components) {
-      if (components.hasOwnProperty(prop)) {
-        // @ts-expect-error
-        const component = components[prop];
-        Vue.component(component.name, component);
-      }
-    }
+    Vue.component("GraphqlSimpleExplorer", GraphqlSimpleExplorer);
+    Vue.component("GraphqlSimpleFieldSelectCheckboxes", FieldSelectCheckboxes);
+    Vue.component("GraphqlSimpleFieldSelectOption", FieldSelectOption);
   },
 };
 
