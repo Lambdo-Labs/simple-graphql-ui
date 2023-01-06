538cafbf651a14378b8efa5efd77c33b6e87e579
Andre Carrera <andre@carrera.red>
2023-01-06 10:33:08 -0700

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import GraphqlSimpleExplorer from "@/components/GraphqlSimpleExplorer.vue";
-import FieldSelectCheckboxes from "@/components/tree/FieldSelectCheckboxes.vue";
-import FieldSelectOption from "@/components/tree/FieldSelectOption.vue";
-import { useGraphqlStore } from "@/composables/GraphqlStore";
+import { useGraphqlStore } from '@/composables/GraphqlStore';
+import GraphqlSimpleExplorer from "./components/GraphqlSimpleExplorer.vue";
+import FieldSelectCheckboxes from "./components/tree/FieldSelectCheckboxes.vue";
+import FieldSelectOption from "./components/tree/FieldSelectOption.vue";
 import type { App } from "vue";
 
-export const simpleExplorer = {
+const install = {
   install(Vue: App) {
     Vue.component("GraphqlSimpleExplorer", GraphqlSimpleExplorer);
     Vue.component("GraphqlSimpleFieldSelectCheckboxes", FieldSelectCheckboxes);
     Vue.component("GraphqlSimpleFieldSelectOption", FieldSelectOption);
   },
+  useGraphqlStore
 };
 
-export useGraphqlStore;
+export default install;
